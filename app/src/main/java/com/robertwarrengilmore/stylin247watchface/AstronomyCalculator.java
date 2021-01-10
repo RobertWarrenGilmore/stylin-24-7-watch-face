6cc1359e7f9a031d35f5ce70ae5e59283256f071
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2021-01-10 09:24:16 -0800

    Added javadoc comment.

--- app/src/main/java/com/robertwarrengilmore/stylin247watchface/AstronomyCalculator.java
+++ app/src/main/java/com/robertwarrengilmore/stylin247watchface/AstronomyCalculator.java
@@ -13,6 +13,9 @@ class AstronomyCalculator {
       Duration.ofDays(29).plusHours(12).plusMinutes(44).plusSeconds(2);
   private static Instant KNOWN_NEW_MOON_INSTANT = Instant.parse("2021-01-13T05:00:00Z");
 
+  /**
+   * Calculates the approximate phase of the moon on a given date. The result is expressed as a float 0 <= x < 1, where 0 is a new moon, 0.5 is a full moon, and 0.9 is a waning crescent.
+   */
   static float getMoonPhase(Instant when) {
     Duration timeSinceKnownNewMoon = Duration.between(KNOWN_NEW_MOON_INSTANT, when);
     long secondsInLunarCycle = LUNAR_CYCLE_LENGTH.getSeconds();
