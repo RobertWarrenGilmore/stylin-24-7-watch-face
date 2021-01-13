599361553261d156bdd9ac1290a22d48cf2b47e2
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2021-01-13 06:46:14 -0800

    WIP Got the settings activity to show up. Padding is still wrong.

--- app/src/main/java/com/robertwarrengilmore/stylin247watchface/Stylin247SettingsActivity.java
+++ app/src/main/java/com/robertwarrengilmore/stylin247watchface/Stylin247SettingsActivity.java
@@ -1,36 +1,18 @@
 package com.robertwarrengilmore.stylin247watchface;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsets;
 
-import androidx.wear.widget.BoxInsetLayout;
+import androidx.fragment.app.FragmentActivity;
 
-public class Stylin247SettingsActivity extends Activity {
+public class Stylin247SettingsActivity extends FragmentActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_stylin247_settings);
-
-    BoxInsetLayout content = findViewById(R.id.content);
-
-    // BoxInsetLayout adds padding by default on round devices. Add some on square devices.
-    content.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
-      @Override
-      public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
-        if (!insets.isRound()) {
-          v.setPaddingRelative((int) getResources().getDimensionPixelSize(R.dimen.content_padding_start),
-              v.getPaddingTop(),
-              v.getPaddingEnd(),
-              v.getPaddingBottom());
-        } else {
-        }
-        return v.onApplyWindowInsets(insets);
-      }
-    });
-
+    getSupportFragmentManager().beginTransaction()
+        .replace(R.id.settings_container, new SettingsFragment())
+        .commit();
 
   }
 }
