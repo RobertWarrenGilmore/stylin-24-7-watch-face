5826fb3a2b10188a97e4de9f14b58b884ec93d08
Robert Warren Gilmore <Robert.Warren.Gilmore@gmail.com>
2021-01-16 16:08:52 -0800

    Improved location update logic.

--- app/src/main/java/com/robertwarrengilmore/stylin247watchface/LocationCache.java
+++ app/src/main/java/com/robertwarrengilmore/stylin247watchface/LocationCache.java
@@ -10,7 +10,10 @@ import androidx.core.app.ActivityCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.Task;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
 
 public class LocationCache {
@@ -19,57 +22,93 @@ public class LocationCache {
   private final Context context;
   private Location cachedLocation;
   private boolean waitingOnLocationTask = false;
-
+  private Instant lastChange;
+  private Instant lastCheck;
+  private static final Duration COOL_DOWN_TIME = Duration.ofSeconds(5);
+  private static final Duration SOFT_REFRESH_DELAY = Duration.ofSeconds(30);
+  private static final Duration HARD_REFRESH_DELAY = Duration.ofMinutes(30);
+  public static final float SATISFACTORY_ACCURACY = 200_000f;
 
   LocationCache(Context context) {
     this.context = context;
     fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
   }
 
-  private boolean needsUpdate() {
-    // TODO Surely we want to update in cases when the location is stale as well.
-    return cachedLocation == null;
+  private boolean shouldChangeLocation(Location newLocation) {
+    if (newLocation == null) {
+      return false;
+    }
+    // We have a new location to consider,
+    if (cachedLocation == null) {
+      return true;
+    }
+    // and there is already a location in the cache,
+    if (lastChange.isBefore(Instant.now().minus(SOFT_REFRESH_DELAY))) {
+      return true;
+    }
+    // which isn't stale yet,
+    if (newLocation.getAccuracy() > SATISFACTORY_ACCURACY) {
+      return false;
+    }
+    // and the new location is reasonably precise,
+    if (cachedLocation.getAccuracy() != 0f &&
+        newLocation.getAccuracy() >= cachedLocation.getAccuracy()) {
+      return true;
+    }
+    // but not as precise as the cached location.
+    return false;
   }
 
   private void asyncUpdate() {
-    if (waitingOnLocationTask) {
-      return;
-    }
     if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) !=
         PackageManager.PERMISSION_GRANTED &&
         ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) !=
             PackageManager.PERMISSION_GRANTED) {
-      System.out.println("No permission!");
       return;
     }
-    System.out.println("Getting location!");
-    fusedLocationClient.getLastLocation()
-        .addOnSuccessListener(this::onLocationTaskSuccess)
-        .addOnCompleteListener(lastLocationTask -> {
-          if (lastLocationTask.isSuccessful() && lastLocationTask.getResult() != null) {
-            waitingOnLocationTask = false;
-            return;
-          }
-          fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_LOW_POWER, null)
-              .addOnSuccessListener(this::onLocationTaskSuccess)
-              .addOnCompleteListener(task -> waitingOnLocationTask = false);
-        });
+    Task<Location> getLocationTask;
+    if (lastChange == null || lastChange.isBefore(Instant.now().minus(HARD_REFRESH_DELAY))) {
+      getLocationTask =
+          fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_LOW_POWER, null);
+    } else {
+      getLocationTask = fusedLocationClient.getLastLocation();
+    }
+    getLocationTask.addOnCompleteListener(lastLocationTask -> {
+      if (lastLocationTask.isSuccessful()) {
+        lastCheck = Instant.now();
+        if (lastLocationTask.getResult() != null) {
+          changeLocation(lastLocationTask.getResult());
+        }
+      }
+      waitingOnLocationTask = false;
+    });
   }
 
-  private void onLocationTaskSuccess(Location newLocation) {
-    // TODO Decide whether to replace the location. https://developer.android.com/training/location/retrieve-current#BestEstimate
-    System.out.println("Got location!");
-    if (newLocation == null) {
+  private void changeLocation(Location newLocation) {
+    if (!shouldChangeLocation(newLocation)) {
       return;
     }
     cachedLocation = newLocation;
+    lastChange = Instant.now();
+  }
+
+  private boolean shouldUpdate() {
+    if (waitingOnLocationTask) {
+      return false;
+    }
+    if (lastCheck == null) {
+      return true;
+    }
+    if (lastCheck.isAfter(Instant.now().minus(COOL_DOWN_TIME))) {
+      return false;
+    }
+    return true;
   }
 
   Optional<Location> getLocation() {
-    if (needsUpdate()) {
+    if (shouldUpdate()) {
       asyncUpdate();
     }
-    System.out.println("Location is " + cachedLocation + ".");
     return Optional.ofNullable(cachedLocation);
   }
 
